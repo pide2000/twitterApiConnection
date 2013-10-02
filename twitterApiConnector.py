253bbfd99cbc1f4c1f55dcb1aa2b72a094833c5f
Peter Bolch <peter@bolch-steinegger.de>
2013-10-02 15:55:18 +0200

    Added DocStrings to use with pydoc. (pydoc twitterApiConnector) Some minor fixes

--- twitterApiConnector.py
+++ twitterApiConnector.py
@@ -1,17 +1,12 @@
+"""
+A Twitter Api Connector
+"""
+
 __author__ = 'Peter Bolch'
 
 #!/usr/bin/env python
 # encoding: utf-8
-"""
-* 	Twitter Api Connection Class
-*   provides so far connections to the following services offered by twitter:
-*   ratelimit
-*   get a bearer token
-*   search
-*   get a tweet by id
-*   twitter streaming api
-*   filter streaming api
-"""
+
 
 import base64
 import json
@@ -21,22 +16,56 @@ import requests
 
 
 class TwitterApiConnector:
+    """
+    Twitter Api Connection Class
+    Provides a simple way to connect to the Twitter Api with your credentials. Visit dev.twitter.com to get the
+    credentials to access the api's. With your credentials automatically a bearer token is fetched from the twitter
+    api. After this you can access the api's simply with the provided methods.
+
+    Attributes:
+        consumer_key:   Twitter Consumer Key
+        consumer_secret:    Twitter Consumer Secret
+        token_key:  Twitter Token Key
+        token_secret: Twitter Token Secret
+
+    Returns:
+        A TwitterApiConnection Object
+    """
 
     def __init__(self, consumer_key, consumer_secret, token_key, token_secret):
+
         self.c_key = consumer_key
         self.c_secret = consumer_secret
         self.t_key = token_key
         self.t_secret = token_secret
         self.bearer_token = self._get_bearer_token()
 
+
+
+    # todo: provide possibility to add params to restrict to specific endpoints
     def get_rate_limit(self):
+        """
+        Get your actual rate limits.
+
+        Returns:
+            A python dict with your current rate limits of ALL Endpoints
+
+        """
         session = rauth.OAuth2Session(self.c_key, self.c_secret, access_token=self.bearer_token)
         tweet = session.get('https://api.twitter.com/1.1/application/rate_limit_status.json')
         #print(json.loads(tweet.text))
         print(tweet.request.headers)
         return json.loads(tweet.text)
 
+
+
     def _get_bearer_token(self):
+        """
+        Gets the bearer token with your credentials
+
+        Returns:
+            A bearer token as string
+        """
         service = rauth.OAuth2Service(name='Peters App',
                                       client_id=self.c_key,
                                       client_secret=self.c_secret,
@@ -57,10 +86,10 @@ class TwitterApiConnector:
         base64_encoded_bearer_token = base64.b64encode(temp_token)
         para = {'Host': 'api.twitter.com',
                 'User-Agent': 'something',
-                'Authorization': 'Basic ' + base64_encoded_bearer_token + '',
+                'Authorization': 'Basic ' + str(base64_encoded_bearer_token) + '',
                 'Accept': '*/*',
                 'Content-Type': 'application/x-www-form-urlencoded'
-        }
+                }
         datat = 'access_token=' + self.bearer_token
         r = requests.post('https://api.twitter.com/oauth2/invalidate_token', data=datat, headers=para)
         print("header")
@@ -69,6 +98,15 @@ class TwitterApiConnector:
         print(r.text)
 
     def search_twitter(self, search_params):
+        """
+        Search for tweets using parameters as specified here: https://dev.twitter.com/docs/api/1.1/get/search/tweets
+
+        Args:
+            search_params: A Python Dictionary with the search parameters
+
+        Returns:
+            A python dict with search results
+        """
         session = rauth.OAuth2Session(self.c_key, self.c_secret, access_token=self.bearer_token)
         tweet = session.get('https://api.twitter.com/1.1/search/tweets.json',
                             params=search_params)
@@ -76,12 +114,31 @@ class TwitterApiConnector:
         return json.loads(tweet.text)
 
     def get_a_tweet(self, tweet_id):
+        """
+        Search for a specific tweet by tweet_id
+
+        Args:
+            tweet_is: the id of the tweet you want to get
+
+        Returns:
+            A python dict with result tweet
+        """
         session = rauth.OAuth2Session(self.c_key, self.c_secret, access_token=self.bearer_token)
         tweet = session.get('https://api.twitter.com/1.1/statuses/show.json',
                             params={'id': tweet_id, 'include_entities': 'true'})
         return json.loads(tweet.text)
 
+    #todo: give it a callback possibility to call on every received tweet. Add a callback argument
     def twitter_stream(self):
+        """
+        gets the twitter stream and prints it on the console
+
+        Args:
+
+
+        Returns:
+            nothing
+        """
         url = "https://stream.twitter.com/1.1/statuses/sample.json"
 
         session = rauth.OAuth1Session(self.c_key, self.c_secret, self.t_key, self.t_secret)
@@ -101,7 +158,16 @@ class TwitterApiConnector:
                 print('an error occured')
                 raise
 
-    def filter_stream(self, filter_params={}):
+    #todo: give it a callback possibility to call on every received tweet. Add a callback argument
+    def filter_stream(self, filter_params={'track': '#killertomatoes'}):
+        """
+        Filter the twitter stream, filter options from https://dev.twitter.com/docs/api/1.1/post/statuses/filter
+        Args:
+            filter_params: A Python Dictionary with the filter parameters
+
+        Returns:
+            prints the results on the console
+        """
         url = "https://stream.twitter.com/1.1/statuses/filter.json"
         session = rauth.OAuth1Session(self.c_key, self.c_secret, self.t_key, self.t_secret)
 
@@ -125,7 +191,11 @@ if __name__ == '__main__':
     CONSUMERSECRET = ''
     TOKENKEY = ''
     TOKENSECRET = ''
-    twitter = TwitterApiConnector(CONSUMERKEY, CONSUMERSECRET, TOKENKEY, TOKENSECRET)
+    #twitter = TwitterApiConnector(CONSUMERKEY, CONSUMERSECRET, TOKENKEY, TOKENSECRET)
     #twitter.grab_stream()
-    print(twitter.get_a_tweet())
-    print(twitter.search_twitter({'q': '#iphone', 'include_entities': 'true'}))
+    #print(twitter.get_a_tweet())
+    #print(twitter.search_twitter({'q': '#iphone', 'include_entities': 'true'}))
+    #'include_entities': 0,
+    #'stall_warning': 'true'
+    #'track': ''
+    #print(twitter.filter_stream())
